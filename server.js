ec076a0fa78b14a0f92894673d685999bbb88c9e
Luca <lucadepetris2@gmail.com>
2025-07-29 22:34:02 -0300

    Arreglos en el server

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 const jsonServer = require('json-server');
 
 const app = express();
-
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
@@ -14,7 +12,6 @@ const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', middlewares, router);
 
@@ -22,6 +19,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+const server = app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
+
+server.keepAliveTimeout = 120000;
+server.headersTimeout = 120000;
+
+
